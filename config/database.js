7d444b72c3724cd4f7f788510603cdf4a2df5a62
nicswebdev <nicholaspratama94s@gmail.com>
2022-11-17 20:25:03 +0800

    Update field

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "test_git"),
+      database: env("DATABASE_NAME", "nusadua_kayumanis"),
       user: env("DATABASE_USERNAME", "root"),
       password: env("DATABASE_PASSWORD", ""),
       ssl: env.bool("DATABASE_SSL", false),
